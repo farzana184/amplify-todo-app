95cf8f0b59e8f77383ab8266425d3e43c70deb45
Farzana <farzana184@hotmail.com>
2025-06-18 12:51:54 +1000

    Add all project files to repository

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// --- IMPORTANT: Place ALL imports at the very top ---
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import './index.css'; // Keep CSS imports here too
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// --- End of import section ---
+
+// --- Configuration and other code comes AFTER all imports ---
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +19,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
